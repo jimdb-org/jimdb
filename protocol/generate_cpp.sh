e03353d5d8bef29a087dcd2bd0e2223bbd7e6bab
byteink <jonahgaox@gmail.com>
2020-01-14 15:51:30 +0800

    update proto import path

--- protocol/generate_cpp.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cp -rf ../proto ../dataserver/src/proto
-mkdir -p ../dataserver/src/proto/gen
-sed -i -E '/gogo/d' `grep -rl gogo ../dataserver/src/proto/proto`
-sed -i -E 's/github.com\/chubaodb\/chubaodb\/proto\///' `grep -rl github.com ../dataserver/src/proto/proto`
-protoc -I../dataserver/src/proto/proto --cpp_out=../dataserver/src/proto/gen ../dataserver/src/proto/proto/*/*.proto
-
-rm -rf ../dataserver/src/proto/proto
--- protocol/generate_java.sh
+++ protocol/generate_java.sh
@@ -5,8 +5,8 @@ cp -rf ../jim-protocol/*pb ../jimsql/jim-sdk/jim-proto/src/main/proto
 cd ../jimsql/jim-sdk/jim-proto/src/main
 
 sed -i '/gogo/d' `grep -rl gogo proto`
-sed -i 's/github.com\/chubaodb\/chubaodb\/proto\///' `grep -rl github.com proto`
-sed -i "/package/a\\option java_package=\"io.chubao.jim.pb\";" proto/*/*.proto
+sed -i 's/github.com\/jimdb-org\/jimdb\/protocol\///' `grep -rl github.com proto`
+sed -i "/package/a\\option java_package=\"io.jimdb.pb\";" proto/*/*.proto
 sed -i "/java_package/a\\option java_outer_classname=\"Exprpb\";" proto/dspb/expr.proto
 sed -i "/java_package/a\\option java_outer_classname=\"Errorpb\";" proto/dspb/error.proto
 
